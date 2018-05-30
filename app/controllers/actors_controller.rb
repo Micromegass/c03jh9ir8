9a8af35be97a6865ae13f7292f52695ff022ee81
axelbraunschweiger <axelbraunschweiger@gmail.com>
2018-05-30 09:57:49 -0500

    created functionality to create actors

--- app/controllers/actors_controller.rb
+++ app/controllers/actors_controller.rb
@@ -6,4 +6,27 @@ class ActorsController < ApplicationController
         render :actors        
     end 
 
+
+    
+    def new 
+        @actor = Actor.new 
+    end 
+    
+    
+    def create 
+        @actor = Actor.create(actors_params)
+        redirect_to actors_path
+    end 
+
+
+
+
+
+     private
+     def actors_params
+        params.require(:actor).permit(:name, :bio, :birth_date, :birth_place, :image_url, :alive, :death_date, :death_place)
+     end 
+
+
 end 
+
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
 
-
   get '/actors', to: 'actors#show'
-
- 
+  get '/new', to: 'actors#new'
+  post '/actors', to: 'actors#create'
+  
 end
